5ae9722f1f0e97c24a0d22f34a4d1ac0c9f11dfc
Sharapidinov <austie2204@gmail.com>
2022-11-19 14:07:06 +0600

    init admin page

---
+++ src/components/AdminPage/AddList/AddList.style.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const AddListContaainer = styled.div`
+
+`
+
+export const AddListUlTtitles = styled.ul`
+
+`
+
+export const AddListItem = styled.li`
+
+`
+
+export const AddListBtnBox = styled.div`
+
+`
+
+export const AddListBtn = styled.button`
+  svg{
+    path{
+      fill: #2A2349;
+      transition: .2s;
+      &:hover{
+        fill: #00CCDB;
+      }
+    }
+  }
+  &:hover{
+    svg {
+      path {
+        fill: #00CCDB;
+      }
+    }
+  }
+`
+
+
+export const AddListUlInfo = styled.ul`
+
+`
+
+
+
