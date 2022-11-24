4cc39a7f049b29286813fef0a7efb4af75bb7a42
theoddorrrrr <aidar140702@mail.ru>
2022-11-24 12:45:50 +0600

    changed search

--- src/components/Header/Search/index.tsx
+++ src/components/Header/Search/index.tsx
@@ -6,20 +6,31 @@ import { SelectCategory } from './Categories/SelectCategory';
 import css from './styles.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
+import axios from 'axios';
 
 export const SearchBlock = () => {
   const [isActiveCategories, setIsActiveCategories] = useState(false);
-  const [category, setCategory] = useState();
+  const [category, setCategory] = useState<{ name: string }>();
   const [value, setValue] = useState('');
   const [isShowSearchResult, setIsShowSearchResult] = useState(false);
-  const categories = [];
+  const categories: number[] = [];
+  const [searchedGoods, setSearchedGoods] = useState([]);
+  console.log(searchedGoods, 'searched goods');
 
-  // const getGoodsBySearch = () => {
-  //   console.log('Goods');
-    
-  // }
+  const getGoodsBySearch = async (value: string) => {
+    console.log(value, 'value');
+    try {
+      const response = await axios(
+        'http://188.225.83.42:8001/api/v1/advertisement/list/'
+      );
+      setSearchedGoods(response.data.results);
+    } catch (e) {
+      console.log(e, 'error');
+    }
+  };
 
   useEffect(() => {
+    getGoodsBySearch(value);
     value && setIsShowSearchResult(true);
   }, [value]);
 
@@ -43,7 +54,7 @@ export const SearchBlock = () => {
               onClick={() => setIsActiveCategories(true)}
               ref={ref}
             >
-              {category?.name ?? ' Во всех категориях'}
+              {category?.name || ' Во всех категориях'}
               <Image
                 src="/header/arrow-down.svg"
                 alt="arrow"
@@ -76,31 +87,57 @@ export const SearchBlock = () => {
               onClick={() => setIsShowSearchResult(true)}
             />
             <div className={css.searchAdvert}>
-              {isShowSearchResult && value ? (
-                <Link
-                  href={`/detail/${72}`}
-                  className={css.item}
-                  onClick={() => setValue('')}
-                >
-                  <div className={css.text}>
-                    <p className={css.title}>{"Aidar"}</p>
-                    <p className={css.number}>{500000} ₸</p>
-                  </div>
-                </Link>
-              ) : // advertsData?.results.map((item, i) => (
-              //     <Link
-              //       href={`/detail/${item.id}`}
-              //       className={css.item}
-              //       key={i}
-              //       onClick={() => setValue('')}
-              //     >
-              //       <div className={css.text}>
-              //         <p className={css.title}>{item.name}</p>
-              //         <p className={css.number}>{item.start_price} ₸</p>
-              //       </div>
-              //     </Link>
-              //   ))
-              null}
+              {isShowSearchResult && value
+                ? searchedGoods.map(
+                    (
+                      item: { id: number; name: string; start_price: number },
+                      i
+                    ) => (
+                      <Link
+                        href={`/detail/${item.id}`}
+                        className={css.item}
+                        key={i}
+                        onClick={() => setValue('')}
+                      >
+                        <Image
+                          src="/main/good.png"
+                          width={40}
+                          height={40}
+                          alt="good"
+                        />
+                        <div className={css.text}>
+                          <p className={css.title}>{item.name}</p>
+                          <p className={css.number}>{item.start_price} ₸</p>
+                        </div>
+                      </Link>
+                    )
+                  )
+                : //  (
+                  //   <Link
+                  //     href={`/detail/${72}`}
+                  //     className={css.item}
+                  //     onClick={() => setValue('')}
+                  //   >
+                  //     <div className={css.text}>
+                  //       <p className={css.title}>{'Aidar'}</p>
+                  //       <p className={css.number}>{500000} ₸</p>
+                  //     </div>
+                  //   </Link>
+                  // )
+                  // advertsData?.results.map((item, i) => (
+                  //     <Link
+                  //       href={`/detail/${item.id}`}
+                  //       className={css.item}
+                  //       key={i}
+                  //       onClick={() => setValue('')}
+                  //     >
+                  //       <div className={css.text}>
+                  //         <p className={css.title}>{item.name}</p>
+                  //         <p className={css.number}>{item.start_price} ₸</p>
+                  //       </div>
+                  //     </Link>
+                  //   ))
+                  null}
             </div>
             <button type="submit" className={css.seacrhBtn}>
               <span>найти</span>
